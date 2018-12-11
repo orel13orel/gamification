521c65e2432180eb661ef58a2039238fb799c7ac
orel13orel <37869345+orel13orel@users.noreply.github.com>
2018-12-11 13:08:12 +0200

    Add files via upload

---
+++ test/test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Studio on 11/12/2018.
+//  Copyright © 2018 Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        print("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
