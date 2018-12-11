b31520706dad346da30f0cc11c0e3713deb1c9a7
Itsik <itsikd8@gmail.com>
2018-12-11 13:25:00 +0200

    bla

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        print("hello22")
+        print("hello2266")
     }
 
     override func didReceiveMemoryWarning() {
