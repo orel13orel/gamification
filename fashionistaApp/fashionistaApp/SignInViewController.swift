b0d1fc148ad7e0df8bc7bdeb5670640ecda42c99
lital opaets <litalopaets@gmail.com>
2018-12-11 17:43:51 +0200

    add print function(check)

--- fashionistaApp/fashionistaApp/SignInViewController.swift
+++ fashionistaApp/fashionistaApp/SignInViewController.swift
@@ -12,7 +12,7 @@ class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+print("hello")
         // Do any additional setup after loading the view.
     }
 
