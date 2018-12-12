3e4e0e607078d5eb6c71e92d3fcd8128c5b152bf
litalopaets <44837422+litalopaets@users.noreply.github.com>
2018-12-12 14:22:59 +0200

    firebase check
    updated sign up view controller with check of uploading specific user to firebase

--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -7,32 +7,43 @@
 //
 
 import UIKit
-
+import  FirebaseAuth
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var FullNameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var EmailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profilePicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        profilePicture.layer.cornerRadius = 40
+        profilePicture.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
     @IBAction func dismiss_onClick(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBAction func CreateMyAccount(_ sender: Any) {
+        Auth.auth().createUser(withEmail: "user@gmail.com", password: "123456") { (user: AuthDataResult?, error: Error?) in
+            if error != nil {
+                print(error?.localizedDescription)
+                return
+            }
+            print(user)
+        }
     }
+    /*@IBAction func signUpBtn(_ sender: Any) {
+        Auth.auth().createUser(withEmail: "user@gmail.com", password: "123456" , completion:  { (user: AuthDataResult?, error: Error?) in
+            if error != nil {
+                print(error?.localizedDescription)
+                return
+            }
+            print(user)
+        })
+}*/
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
