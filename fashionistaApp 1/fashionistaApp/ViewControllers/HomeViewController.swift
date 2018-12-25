00103bf474591c3b2b76e244e45acbf021cb0a71
daniel <leibman.daniel@gmail.com>
2018-12-25 17:30:34 +0200

    HomeViewController
    prototype cells

--- fashionistaApp 1/fashionistaApp/ViewControllers/HomeViewController.swift
+++ fashionistaApp 1/fashionistaApp/ViewControllers/HomeViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var TabView: UITableView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    TabView.dataSource = self
+        LoadPost()
+    }
+    
+    func LoadPost() {
+        Database.database().reference().child("Posts").observe(.childAdded) { (snapshot: DataSnapshot) in
+            print(snapshot.value!)
+        }
+    }
+    
+}
 
-        // Do any additional setup after loading the view.
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+         cell.backgroundColor = UIColor.red
+        return cell
     }
 }
