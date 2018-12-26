23e1f7d221d6d22f272d476d2ec2e5a2203733c4
itsik <itsikd8@gmail.com>
2018-12-26 13:23:28 +0200

    Post configuration MVC model

---
+++ fashionistaApp 1/fashionistaApp/Model/Post.swift
@@ -0,0 +1,26 @@
+//
+//  Post.swift
+//  fashionistaApp
+//
+//  Created by admin on 25/12/2018.
+//  Copyright © 2018 Studio. All rights reserved.
+//
+
+import Foundation
+
+
+class Post{
+    
+    var caption : String?
+    var photoUrl : String?
+    
+}
+
+extension Post {
+    static func TransformPostPhoto(dict: [String: Any])-> Post {
+        let post = Post()
+        post.caption = dict["caption"] as? String
+        post.photoUrl = dict["photoUrl"] as? String
+        return post
+    }
+}
--- fashionistaApp 1/fashionistaApp/ViewControllers/HomeViewController.swift
+++ fashionistaApp 1/fashionistaApp/ViewControllers/HomeViewController.swift
@@ -11,32 +11,39 @@ import FirebaseDatabase
 import FirebaseAuth
 
 class HomeViewController: UIViewController {
-
+    
     @IBOutlet weak var TabView: UITableView!
-   
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    TabView.dataSource = self
+        
+        TabView.dataSource = self
         LoadPost()
+        
     }
     
     func LoadPost() {
         Database.database().reference().child("Posts").observe(.childAdded) { (snapshot: DataSnapshot) in
-            print(snapshot.value!)
+            print(Thread.isMainThread)
+            if let dict = snapshot.value as? [String: Any] {
+                let NewPost = Post.TransformPostPhoto(dict: dict)
+                self.posts.append(NewPost)
+                self.TabView.reloadData()
+            }
         }
     }
-    
 }
 
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return posts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
-         cell.backgroundColor = UIColor.red
+        cell.textLabel?.text = posts[indexPath.row].caption
         return cell
     }
 }
