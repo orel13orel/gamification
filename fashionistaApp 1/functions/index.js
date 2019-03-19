35f8aad5bde9d1663a067a561f77fbdcc6144135
orel13orel <orel13orel@gmail.com>
2019-03-19 13:14:47 +0200

    addRule+change name for older funcs

--- fashionistaApp 1/functions/index.js
+++ fashionistaApp 1/functions/index.js
@@ -61,15 +61,16 @@ exports.addBadgeToSet=functions.https.onRequest((req,res)=>{
         return res.redirect(303, snapshot.ref.toString());
     });
 });
+
 //receives existing context id and new action name, return the new action id
-exports.addActionToSet = functions.https.onRequest((req,res)=>{
+exports.addAction = functions.https.onRequest((req,res)=>{
     let arr = req.query.text.split("/");
     let context_id=arr[0];
     let action_name=arr[1];
     console.log("arr : "+arr);
     console.log("context id : "+context_id);
     console.log("action name : "+action_name);
-    return admin.database().ref('/Rp/Context/'+context_id.toString()+'/////action').push({name: action_name}).then((snapshot) => {
+    return admin.database().ref('/Rp/Context/'+context_id.toString()+'/////Action').push({name: action_name}).then((snapshot) => {
         // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
         let arr2=snapshot.toString().split("/");
         console.log("arr2 : "+arr2);
@@ -77,6 +78,8 @@ exports.addActionToSet = functions.https.onRequest((req,res)=>{
         return res.redirect(303, arr2[5]);
     });
 });
+
+
 //receives new context name, return the context id
 exports.addContextToSet = functions.https.onRequest((req,res)=>{
     const context = req.query.text;
@@ -93,64 +96,87 @@ exports.addContextToSet = functions.https.onRequest((req,res)=>{
     });
 });
 
+//
+exports.addRule = functions.https.onRequest((req,res)=>{
+    let arr = req.query.text.split("/");
+    let context_id=arr[0];
+    let action_id=arr[1];
+    let from=arr[2];
+    let to =arr[3];
+    let point_id=arr[4];
 
+    return admin.database().ref('/Rp/Context/'+context_id.toString()+'/////Action/'+action_id.toString()+'/////Rule').push({from: from , to:to , points: point_id}).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        let arr2=snapshot.toString().split("/");
+        console.log("context_id =" + context_id);
+        console.log("action_id =" + action_id);
+        console.log("from =" + from);
+        console.log("to =" + to);
+        console.log("point_id =" + point_id);
 
-// Saves a message to the Firebase Realtime Database but sanitizes the text by removing swearwords.
-exports.addMessage2 = functions.https.onCall((data, context) => {
-    // Message text passed from the client.
-    const text = data.text;
-// Authentication / user information is automatically added to the request.
-    const uid = context.auth.uid;
-    const name = context.auth.token.name || null;
-    const picture = context.auth.token.picture || null;
-    const email = context.auth.token.email || null;
-
-    // Checking attribute.
-    if (!(typeof text === 'string') || text.length === 0) {
-        // Throwing an HttpsError so that the client gets the error details.
-        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
-            'one arguments "text" containing the message text to add.');
-    }
-// Checking that the user is authenticated.
-    if (!context.auth) {
-        // Throwing an HttpsError so that the client gets the error details.
-        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
-            'while authenticated.');
-    }
-
-
-    // Saving the new message to the Realtime Database.
-    const sanitizedMessage = sanitizer.sanitizeText(text); // Sanitize the message.
-    return admin.database().ref('/messages').push({
-        text: sanitizedMessage,
-        author: { uid, name, picture, email },
-    }).then(() => {
-        console.log('New Message written');
-        // Returning the sanitized message to the client.
-        return { text: sanitizedMessage };
-    })
-
-
-
-
-});
-
-exports.addMessage3 = functions.https.onCall((data, context) => {
-
-    const mac = data.text;
-    console.log(data);
-
-
-
-    //add MAC to table
-    admin.database().ref('/MAC').push({context: mac});
-
-
-    return admin.database().ref('/MAC').push({context: mac}).then((snapshot) => {
-        console.log('New Message written');
-        return{text: 'New Message written'}
+        console.log("arr2 : "+arr2);
+        console.log("rule id : "+arr2[9]);
+        return res.redirect(303, arr2[9]);
     });
 });
+
+
+// // Saves a message to the Firebase Realtime Database but sanitizes the text by removing swearwords.
+// exports.addMessage2 = functions.https.onCall((data, context) => {
+//     // Message text passed from the client.
+//     const text = data.text;
+// // Authentication / user information is automatically added to the request.
+//     const uid = context.auth.uid;
+//     const name = context.auth.token.name || null;
+//     const picture = context.auth.token.picture || null;
+//     const email = context.auth.token.email || null;
+//
+//     // Checking attribute.
+//     if (!(typeof text === 'string') || text.length === 0) {
+//         // Throwing an HttpsError so that the client gets the error details.
+//         throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+//             'one arguments "text" containing the message text to add.');
+//     }
+// // Checking that the user is authenticated.
+//     if (!context.auth) {
+//         // Throwing an HttpsError so that the client gets the error details.
+//         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+//             'while authenticated.');
+//     }
+//
+//
+//     // Saving the new message to the Realtime Database.
+//     const sanitizedMessage = sanitizer.sanitizeText(text); // Sanitize the message.
+//     return admin.database().ref('/messages').push({
+//         text: sanitizedMessage,
+//         author: { uid, name, picture, email },
+//     }).then(() => {
+//         console.log('New Message written');
+//         // Returning the sanitized message to the client.
+//         return { text: sanitizedMessage };
+//     })
+//
+//
+//
+//
+// });
+
+// exports.addMessage3 = functions.https.onCall((data, context) => {
+//
+//     const mac = data.text;
+//     console.log(data);
+//
+//
+//
+//     //add MAC to table
+//     admin.database().ref('/MAC').push({context: mac});
+//
+//
+//     return admin.database().ref('/MAC').push({context: mac}).then((snapshot) => {
+//         console.log('New Message written');
+//         return{text: 'New Message written'}
+//     });
+// });
 
 
 
