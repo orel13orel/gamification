5292043375c0535a324eee503dc52dea4194465b
orel13orel <orel13orel@gmail.com>
2019-03-19 13:58:20 +0200

    temp

--- fashionistaApp 1/functions/index.js
+++ fashionistaApp 1/functions/index.js
@@ -16,32 +16,32 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        return res.redirect(303, snapshot.ref.toString());
-    });
-});
+// // Take the text parameter passed to this HTTP endpoint and insert it into the
+// // Realtime Database under the path /messages/:pushId/original
+// exports.addMessage = functions.https.onRequest((req, res) => {
+//     // Grab the text parameter.
+//     const original = req.query.text;
+//     // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//     return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+//         // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//         return res.redirect(303, snapshot.ref.toString());
+//     });
+// });
 
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onCreate((snapshot, context) => {
-        // Grab the current value of what was written to the Realtime Database.
-        const original = snapshot.val();
-        console.log('Uppercasing', context.params.pushId, original);
-        const uppercase = original.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        return snapshot.ref.parent.child('uppercase').set(uppercase);
-    });
+//
+// // Listens for new messages added to /messages/:pushId/original and creates an
+// // uppercase version of the message to /messages/:pushId/uppercase
+// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+//     .onCreate((snapshot, context) => {
+//         // Grab the current value of what was written to the Realtime Database.
+//         const original = snapshot.val();
+//         console.log('Uppercasing', context.params.pushId, original);
+//         const uppercase = original.toUpperCase();
+//         // You must return a Promise when performing asynchronous tasks inside a Functions such as
+//         // writing to the Firebase Realtime Database.
+//         // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+//         return snapshot.ref.parent.child('uppercase').set(uppercase);
+//     });
 
 exports.addPointToSet = functions.https.onRequest((req,res)=>{
     const point = req.query.text;
@@ -81,7 +81,7 @@ exports.addAction = functions.https.onRequest((req,res)=>{
 
 
 //receives new context name, return the context id
-exports.addContextToSet = functions.https.onRequest((req,res)=>{
+exports.addContext = functions.https.onRequest((req,res)=>{
     const context = req.query.text;
 
     return admin.database().ref('/Rp/Context').push({name: context}).then((snapshot) => {
@@ -96,7 +96,7 @@ exports.addContextToSet = functions.https.onRequest((req,res)=>{
     });
 });
 
-//
+//receives context_id, action_id, range(from -to), point_id. return rule id
 exports.addRule = functions.https.onRequest((req,res)=>{
     let arr = req.query.text.split("/");
     let context_id=arr[0];
@@ -118,6 +118,13 @@ exports.addRule = functions.https.onRequest((req,res)=>{
         console.log("rule id : "+arr2[9]);
         return res.redirect(303, arr2[9]);
     });
+});
+
+//Rp receives M,A,C.
+exports.Rp = functions.https.onRequest((req,res)=>{
+
+
+
 });
 
 
