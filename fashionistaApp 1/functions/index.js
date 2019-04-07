cd347c235beb2b6f8f0a5516000fb632aab86d9f
orel13orel <orel13orel@gmail.com>
2019-04-07 15:44:31 +0300

    add table functions

--- fashionistaApp 1/functions/index.js
+++ fashionistaApp 1/functions/index.js
@@ -43,147 +43,154 @@ admin.initializeApp();
 //         return snapshot.ref.parent.child('uppercase').set(uppercase);
 //     });
 
-exports.addPointToSet = functions.https.onRequest((req,res)=>{
-    const point = req.query.text;
+// exports.addPointToSet = functions.https.onRequest((req,res)=>{
+//     const point = req.query.text;
+//
+//     return admin.database().ref('/Points').push({point: point}).then((snapshot) => {
+//         // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//         return res.redirect(303, snapshot.ref.toString());
+//     });
+// });
 
-    return admin.database().ref('/Points').push({point: point}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        return res.redirect(303, snapshot.ref.toString());
+
+//receives new challenge_name, badge_id, start_time, end_time,points. returns the new challenge_id.
+exports.addchallenge=functions.https.onRequest((req,res)=>{
+    let arr=req.query.text.split("/");
+    const challenge_name=arr[0];
+    const badge_id = arr[1];
+    const strat_time = arr[2];
+    const end_time = arr[3];
+    const points = arr[4];
+    return admin.database().ref('/Challenge/').push({name: challenge_name ,badge_id:badge_id ,strat_time:strat_time, end_time: end_time, points:points}).then((snapshot) => {
+        let arr2 = snapshot.toString().split("/");
+        //console.log("arr2 : "+arr2);
+        //console.log("arr2[4] : "+arr2[4]);
+        return res.redirect(303, arr2[4]);
     });
 });
 
-exports.addBadgeToSet=functions.https.onRequest((req,res)=>{
-    var temp=req.query.text.split(",");
-    const  caption=temp[0];
-    const photoUrl = temp[1];
-    return admin.database().ref('/Badges').push({caption: caption , photoUrl: photoUrl}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        return res.redirect(303, snapshot.ref.toString());
-    });
-});
-
-//receives existing context id and new action name, return the new action id
-exports.addAction = functions.https.onRequest((req,res)=>{
+//receives challenge_id ,action_id, amount. returns the new ActionToChallenge_id
+exports.addActionToChallenge = functions.https.onRequest((req,res)=>{
     let arr = req.query.text.split("/");
-    let context_id=arr[0];
-    let action_name=arr[1];
-    console.log("arr : "+arr);
-    console.log("context id : "+context_id);
-    console.log("action name : "+action_name);
-    return admin.database().ref('/Rp/Context/'+context_id.toString()+'/////Action').push({name: action_name}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        let arr2=snapshot.toString().split("/");
-        console.log("arr2 : "+arr2);
-        console.log("action id : "+arr2[5]);
-        return res.redirect(303, arr2[5]);
+    let challenge_id=arr[0];
+    let action_id=arr[1];
+    let amount=arr[2];
+
+    return admin.database().ref('/Challenge/'+challenge_id.toString()+'/////Actions/').push({action_id: action_id,amount:amount}).then((snapshot) => {
+
+        let arr=snapshot.toString().split("/");
+        console.log(arr[6]);
+        return res.redirect(303, arr[6]);
     });
 });
+
+//receives new badge_name, context_id, isPermanent, photoURL. returns the new badge_id.
+exports.addBadge=functions.https.onRequest((req,res)=>{
+    let arr=req.query.text.split("/");
+    const badge_name=arr[0];
+    const context_id = arr[1];
+    const isPermanent = arr[2];
+    const photoUrl = arr[3];
+    return admin.database().ref('/Badges/').push({name: badge_name ,context_id:context_id ,isPermanent:isPermanent, photoUrl: photoUrl}).then((snapshot) => {
+        let arr2 = snapshot.toString().split("/");
+        console.log("arr2 : "+arr2);
+        console.log("arr2[4] : "+arr2[4]);
+        return res.redirect(303, arr2[4]);
+    });
+});
+
+
 
 
 //receives new context name, return the context id
 exports.addContext = functions.https.onRequest((req,res)=>{
     const context = req.query.text;
 
-    return admin.database().ref('/Rp/Context').push({name: context}).then((snapshot) => {
+    return admin.database().ref('/Context').push({name: context}).then((snapshot) => {
         // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
         //console.log(snapshot);
 
         let arr=snapshot.toString().split("/");
-
-        let context_id=arr[5];
-       // console.log(context_id);
+       // console.log(arr);
+        let context_id=arr[4];
+      // console.log(context_id);
         return res.redirect(303, context_id);
     });
 });
 
-//receives context_id, action_id, range(from -to), point_id. return rule id
-exports.addRule = functions.https.onRequest((req,res)=>{
+//receives new action name and context id, return the action id
+exports.addAction = functions.https.onRequest((req,res)=>{
     let arr = req.query.text.split("/");
-    let context_id=arr[0];
-    let action_id=arr[1];
-    let from=arr[2];
-    let to =arr[3];
-    let point_id=arr[4];
+    let action_name=arr[0];
+    let context_id=arr[1];
 
-    return admin.database().ref('/Rp/Context/'+context_id.toString()+'/////Action/'+action_id.toString()+'/////Rule').push({from: from , to:to , points: point_id}).then((snapshot) => {
+    return admin.database().ref('/Context/'+context_id.toString()+'/////Action/').push({name: action_name}).then((snapshot) => {
         // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        let arr2=snapshot.toString().split("/");
-        console.log("context_id =" + context_id);
-        console.log("action_id =" + action_id);
-        console.log("from =" + from);
-        console.log("to =" + to);
-        console.log("point_id =" + point_id);
+        //console.log(snapshot);
 
-        console.log("arr2 : "+arr2);
-        console.log("rule id : "+arr2[9]);
-        return res.redirect(303, arr2[9]);
+        let arr=snapshot.toString().split("/");
+        console.log(arr);
+        let action_id=arr[6];
+         console.log(action_id);
+        return res.redirect(303, action_id);
     });
 });
 
-//Rp receives M,A,C.
-exports.Rp = functions.https.onRequest((req,res)=>{
 
+
+//receives  action_id, value, points. return rule id
+//Rp table
+exports.addRp = functions.https.onRequest((req,res)=>{
+    let arr = req.query.text.split("/");
+    let action_id=arr[0];
+    let value=arr[1];
+    let points=arr[2];
+
+
+    return admin.database().ref('/Rp/').push({action_id: action_id , value:value , points: points}).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        let arr2=snapshot.toString().split("/");
+        // console.log("arr2 : "+arr2);
+        // console.log("rule id : "+arr2[4]);
+        return res.redirect(303, arr2[4]);
+    });
+});
+
+//receives  badge_id, valueOfPoints, time(in hr). return rule id
+//Rb table
+exports.addRb = functions.https.onRequest((req,res)=>{
+    let arr = req.query.text.split("/");
+    let badge_id=arr[0];
+    let valueOfPoints=arr[1];
+    let time=arr[2];
+
+
+    return admin.database().ref('/Rb/').push({badge_id: badge_id , valueOfPoints:valueOfPoints , time: time}).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        let arr2=snapshot.toString().split("/");
+        // console.log("arr2 : "+arr2);
+        // console.log("rule id : "+arr2[4]);
+        return res.redirect(303, arr2[4]);
+    });
+});
+
+// receives user_id,action_id,context_id.
+exports.addUserActivity = functions.https.onCall((data, context) => {
+    let arr = data.text.split("/");
+    let user_id=arr[0];
+    let action_id=arr[1];
+    let context_id=arr[2];
+    //get the action count for the user
+    let count=admin.database().ref('/User/Action/'+action_id).get('Count');
 
 
 });
 
-
-// // Saves a message to the Firebase Realtime Database but sanitizes the text by removing swearwords.
-// exports.addMessage2 = functions.https.onCall((data, context) => {
-//     // Message text passed from the client.
-//     const text = data.text;
-// // Authentication / user information is automatically added to the request.
-//     const uid = context.auth.uid;
-//     const name = context.auth.token.name || null;
-//     const picture = context.auth.token.picture || null;
-//     const email = context.auth.token.email || null;
-//
-//     // Checking attribute.
-//     if (!(typeof text === 'string') || text.length === 0) {
-//         // Throwing an HttpsError so that the client gets the error details.
-//         throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
-//             'one arguments "text" containing the message text to add.');
-//     }
-// // Checking that the user is authenticated.
-//     if (!context.auth) {
-//         // Throwing an HttpsError so that the client gets the error details.
-//         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
-//             'while authenticated.');
-//     }
-//
-//
-//     // Saving the new message to the Realtime Database.
-//     const sanitizedMessage = sanitizer.sanitizeText(text); // Sanitize the message.
-//     return admin.database().ref('/messages').push({
-//         text: sanitizedMessage,
-//         author: { uid, name, picture, email },
-//     }).then(() => {
-//         console.log('New Message written');
-//         // Returning the sanitized message to the client.
-//         return { text: sanitizedMessage };
-//     })
-//
-//
-//
-//
-// });
-
-// exports.addMessage3 = functions.https.onCall((data, context) => {
-//
-//     const mac = data.text;
-//     console.log(data);
-//
-//
-//
-//     //add MAC to table
-//     admin.database().ref('/MAC').push({context: mac});
-//
-//
-//     return admin.database().ref('/MAC').push({context: mac}).then((snapshot) => {
-//         console.log('New Message written');
-//         return{text: 'New Message written'}
+// exports.exmple1 = functions.https.onRequest(()=>{
+//     admin.database().ref("/Points/-LaLQsQFUVwKvzJE7G_R").on("value",function (snapshot) {
+//         console.log("answer " +snapshot.val());
+//     }, function (errorObject) {
+//         console.log("The read failed: " + errorObject.code);
 //     });
+//
 // });
-
-
-
