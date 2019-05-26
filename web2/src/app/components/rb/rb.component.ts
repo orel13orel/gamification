b38414acaa20d724a4098ecc0ad8be0d54f3fed8
Unknown <orel13orel@cs.colman.ac.il>
2019-05-26 20:00:53 +0300

    add and delete Rb

--- web2/src/app/components/rb/rb.component.ts
+++ web2/src/app/components/rb/rb.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {AngularFireDatabase} from '@angular/fire/database';
+import {NewContext} from '../context/context.component';
+
+export class NewRb {
+  badge_id: string;
+  context: string;
+  valueOfPoints: string;
+}
 
 @Component({
   selector: 'app-rb',
@@ -9,11 +16,20 @@ import {AngularFireDatabase} from '@angular/fire/database';
 })
 export class RbComponent implements OnInit {
   rb: Observable<any[]>;
+  public newRb: NewRb;
+  public delRb: string;
+
   constructor(private db: AngularFireDatabase) {
     this.rb = db.list('Rb').snapshotChanges();
+    this.newRb = new NewRb();
   }
 
   ngOnInit() {
   }
-
+  addRb() {
+    this.db.list('/Rb').push(this.newRb);
+  }
+  deleteRb() {
+    this.db.list('/Context').remove(this.delRb);
+  }
 }
