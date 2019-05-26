c0e341c563c81503743c22bf938f9abfcdebc64c
Unknown <orel13orel@cs.colman.ac.il>
2019-05-26 20:55:26 +0300

    add and delete challenge

--- web2/src/app/components/challenge/challenge.component.ts
+++ web2/src/app/components/challenge/challenge.component.ts
@@ -1,6 +1,22 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AngularFireDatabase} from '@angular/fire/database';
+import {NewRb} from '../rb/rb.component';
+
+export class NewChallenge {
+  badge_id: string;
+  end_time: string;
+  name: string;
+  points: string;
+  start_time: string;
+  constructor() {
+    this.badge_id = '';
+    this.end_time = '';
+    this.name = '';
+    this.points = '';
+    this.start_time = new Date().toDateString();
+  }
+}
 
 @Component({
   selector: 'app-challenge',
@@ -10,8 +26,12 @@ import {AngularFireDatabase} from '@angular/fire/database';
 export class ChallengeComponent implements OnInit {
   challenge: Observable<any[]>;
   @Output() clicked = new EventEmitter<string>();
+  public newChallenge: NewChallenge;
+  public delChallenge: string;
+
   constructor(private db: AngularFireDatabase) {
     this.challenge = db.list('Challenge').snapshotChanges();
+    this.newChallenge = new NewChallenge();
   }
 
   ngOnInit() {
@@ -21,6 +41,14 @@ export class ChallengeComponent implements OnInit {
     // this.contextID = key;
     // console.log(this.contextID);
     this.clicked.emit(key);
+  }
+
+  addChallenge() {
+    console.log(this.newChallenge.end_time);
+    this.db.list('/Challenge').push(this.newChallenge);
+  }
+  deleteChallenge() {
+    this.db.list('/Challenge').remove(this.delChallenge);
   }
 
 }
