dc77fac6bfe8098a383801de2cbd69dbd77ffd92
Unknown <orel13orel@cs.colman.ac.il>
2019-05-27 11:10:26 +0300

    add and delete challenge action

--- web2/src/app/components/challenge-action/challenge-action.component.ts
+++ web2/src/app/components/challenge-action/challenge-action.component.ts
@@ -1,7 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {AngularFireDatabase} from '@angular/fire/database';
 import {Observable} from 'rxjs';
+import {NewAction} from '../action/action.component';
 
+export class NewChallengeAction {
+  action_id: string;
+  amount: string;
+}
 @Component({
   selector: 'app-challenge-action',
   templateUrl: './challenge-action.component.html',
@@ -9,15 +14,33 @@ import {Observable} from 'rxjs';
 })
 export class ChallengeActionComponent implements OnInit {
   challengeAction: Observable<any[]>;
+  public newChallengeAction: NewChallengeAction;
+  public delChallengeAction: string;
+  challengeRef: string;
   // @Input() childContextID: string;
-  constructor( private db: AngularFireDatabase) { }
+  constructor( private db: AngularFireDatabase) {
+    this.newChallengeAction = new NewChallengeAction();
+  }
 
   ngOnInit() {
    // this.challengeAction = this.db.list('Context/' + this.childContextID + '/Action').snapshotChanges();
   }
   public showChallengeAction(event: string) {
     this.challengeAction = this.db.list('Challenge/' + event + '/Actions').snapshotChanges();
+    this.challengeRef = event;
   }
+
+
+  addChallengeAction() {
+    this.db.list('/Challenge/' + this.challengeRef + '/Actions').push(this.newChallengeAction);
+    console.log(this.challengeRef);
+
+  }
+
+  deleteChallengeAction() {
+    this.db.list('/Challenge/' + this.challengeRef + '/Actions').remove(this.delChallengeAction);
+  }
+
 
 
 }
