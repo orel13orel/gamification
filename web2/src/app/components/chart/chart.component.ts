69fa0f4d37d4ea239d02648ae0724cdad4a1fea7
Unknown <orel13orel@cs.colman.ac.il>
2019-05-28 18:19:47 +0300

    chart in progress

--- web2/src/app/components/chart/chart.component.ts
+++ web2/src/app/components/chart/chart.component.ts
@@ -1,17 +1,45 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Chart} from 'chart.js';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+
+export class UserActivity {
+  points: any;
+  action_id: any;
+  context_id: any;
+  user_id: any;
+  // date: any;
+  constructor(points: any , action_id: any , context_id: any , user_id: any)
+}
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
+
 export class ChartComponent implements OnInit {
+  ref: AngularFireList<any>;
+  chartData = null;
   @ViewChild('valueBarsCanvas') valueBarsCanvas;
   valueBarsChart: any;
-  constructor() { }
+  // chartArr: UserActivity[] = [];
+  chartArr: Map<string , UserActivity>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.chartArr = new Map<string, UserActivity>();
+  }
 
   ngOnInit() {
+    this.ref = this.db.list('UserActivity');
+    // , ref => ref.orderByChild('date'));
+    this.ref.valueChanges().subscribe(result => {
+      console.log(result);
+      for (const us of result) {
+        if (this.chartArr[us.date]) {
+
+        }
+      }
+    });
     this.valueBarsChart = new Chart(this.valueBarsCanvas.nativeElement, {
 ///
       type: 'bar',
